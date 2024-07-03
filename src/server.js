c995189fab3af51cb1c0ce31e0e69e5c4e1bc68d
Dutrak <fernandodutra100701@gmail.com>
2024-07-03 20:27:27 -0300

    feat: update server file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 import http from 'node:http'
+import { routes } from './routes.js'
+import { json } from './middlewares/json.js'
 
-const server = http.createServer((req, res) => {
-  res.end('Hello world')
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  await json(req, res)
+
+  const route = routes.find((route) => {
+    return route.method === method && route.path === url
+  })
+
+  if (route) return route.handler(req, res)
+
+  return res.writeHead(401).end('Not Found')
 })
 
 server.listen(3333)
