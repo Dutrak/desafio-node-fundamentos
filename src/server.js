1b0e58307ce3913929839efd519f088c564043de
Dutrak <fernandodutra100701@gmail.com>
2024-07-03 21:50:59 -0300

    fix:update server file to read params

--- src/server.js
+++ src/server.js
@@ -8,10 +8,18 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   const route = routes.find((route) => {
-    return route.method === method && route.path === url
+    return route.method === method && route.path.test(url)
   })
 
-  if (route) return route.handler(req, res)
+  if (route) {
+    const routeParams = req.url.match(route.path)
+
+    const { ...params } = routeParams.groups
+
+    req.params = params
+
+    return route.handler(req, res)
+  }
 
   return res.writeHead(401).end('Not Found')
 })
