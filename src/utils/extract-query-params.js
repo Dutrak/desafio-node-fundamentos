97ef29c795d865e6961df6b9dd4ab52e5aa55304
Dutrak <fernandodutra100701@gmail.com>
2024-07-05 21:08:54 -0300

    fix:rename file

--- src/utils/extract-query-params.js
+++ src/utils/extract-query-params.js
@@ -1,4 +1,4 @@
-export function exctractQueryParams(query) {
+export function extractQueryParams(query) {
   return query.substr(1).split('&').reduce((queryparams, param) => {
     const [key, value] = param.split('=')
     queryparams[key] = value
