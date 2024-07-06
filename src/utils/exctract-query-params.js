0f6ab74997d1a5128b4b4ae31e5bd6e6dafb00ad
Dutrak <fernandodutra100701@gmail.com>
2024-07-05 21:08:35 -0300

    feat:add extract query function

---
+++ src/utils/exctract-query-params.js
@@ -0,0 +1,7 @@
+export function exctractQueryParams(query) {
+  return query.substr(1).split('&').reduce((queryparams, param) => {
+    const [key, value] = param.split('=')
+    queryparams[key] = value
+    return queryparams
+  }, {})
+}
